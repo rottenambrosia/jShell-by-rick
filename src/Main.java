15be6ddbccf456ed3fbbf6b166b07bfdf5ce2763
rottenambrosia <sagnik2005sarkar007@gmail.com>
2026-08-18 22:49:52 +0530

    takes relative paths now

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,14 +8,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         System.out.println(ANSI_CYAN +
-                "                       \n" +
-                        "   _ _____ _       _ _ \n" +
-                        "  |_|   __| |_ ___| | |\n" +
-                        "  | |__   |   | -_| | |\n" +
-                        " _| |_____|_|_|___|_|_|\n" +
-                        "|___|                  "
+                "                      _         \n" +
+                "   ___ _ __ _   _ ___| |_ __ _  \n" +
+                "  / __| '__| | | / __| __/ _` | \n" +
+                " | (__| |  | |_| \\__ \\ || (_| | \n" +
+                "  \\___|_|   \\__,_|___/\\__\\__,_| \n" +
+                "                                "
         + ANSI_RESET);
-        System.out.println("Java Interactive Shell - rick");
+        System.out.println("Crusta Interactive Shell \n" +
+                " - rick\n" +
+                "Hope you like it. (or something like that)");
         Scanner scanner = new Scanner(System.in);
         String cwd =  System.getProperty("user.dir");
         while (true) {
@@ -39,7 +40,7 @@ public class Main {
             }
             // command : type
             if (tokens[0].equals("type")) {
-                type(userInput.substring(5));
+                type(userInput.substring(5), cwd);
                 continue;
             }
             // command : pwd -> so you don't get lost in the scary alleyways
@@ -85,31 +86,99 @@ public class Main {
         }
         return false;
     }
-    private static File locateExecutable (String command) {
+    // private static File locateExecutable (String command) {
+    //     String path = System.getenv("PATH");
+    //     String[] pathDir =  path.split(File.pathSeparator);
+    //     for (String dir : pathDir) {
+    //         File file = new File(dir, command);
+    //         if (file.isFile() &&  file.canExecute()) {
+    //             return file;
+    //         }
+    //         // Useful on Windows when user enters, for example, "git"
+    //         if (System.getProperty("os.name").toLowerCase().contains("win")) {
+    //             File exeFile = new File(dir, command + ".exe");
+    //             if (exeFile.isFile()) {
+    //                 return exeFile;
+    //             }
+    //         }
+    //     }
+    //     return null;
+    // }
+    // private static void type(String command) {
+    //     if (isBuiltin(command)) {
+    //         System.out.println(command + " is a shell builtin");
+    //     } else if (locateExecutable(command) != null) {
+    //         System.out.println(command + " is " + locateExecutable(command).getAbsolutePath());
+    //     } else {
+    //         System.out.println(command + " : not found");
+    //     }
+    //     return;
+    // }
+
+        private static File locateExecutable(String command) {
+        return locateExecutable(command, System.getProperty("user.dir"));
+    }
+
+    private static File locateExecutable(String command, String cwd) {
+        if (command == null || command.isBlank()) return null;
+        File f = new File(command);
+        // Absolute path provided
+        if (f.isAbsolute()) {
+            if (f.isFile() && f.canExecute()) return f;
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                File exe = new File(command + ".exe");
+                if (exe.isFile()) return exe;
+                File bat = new File(command + ".bat");
+                if (bat.isFile()) return bat;
+                File cmd = new File(command + ".cmd");
+                if (cmd.isFile()) return cmd;
+            }
+            return null;
+        }
+        // Relative path (contains separator or starts with dot)
+        if (command.contains(File.separator) || command.startsWith(".")) {
+            File rel = new File(cwd, command);
+            if (rel.isFile() && rel.canExecute()) return rel;
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                File exe = new File(rel.getPath() + ".exe");
+                if (exe.isFile()) return exe;
+                File bat = new File(rel.getPath() + ".bat");
+                if (bat.isFile()) return bat;
+                File cmd = new File(rel.getPath() + ".cmd");
+                if (cmd.isFile()) return cmd;
+            }
+            return null;
+        }
+        // Otherwise search PATH
         String path = System.getenv("PATH");
-        String[] pathDir =  path.split(File.pathSeparator);
+        if (path == null) return null;
+        String[] pathDir = path.split(File.pathSeparator);
         for (String dir : pathDir) {
             File file = new File(dir, command);
-            if (file.isFile() &&  file.canExecute()) {
+            if (file.isFile() && file.canExecute()) {
                 return file;
             }
-            // Useful on Windows when user enters, for example, "git"
             if (System.getProperty("os.name").toLowerCase().contains("win")) {
                 File exeFile = new File(dir, command + ".exe");
-                if (exeFile.isFile()) {
-                    return exeFile;
-                }
+                if (exeFile.isFile()) return exeFile;
+                File bat = new File(dir, command + ".bat");
+                if (bat.isFile()) return bat;
+                File cmd = new File(dir, command + ".cmd");
+                if (cmd.isFile()) return cmd;
             }
         }
         return null;
     }
-    private static void type(String command) {
+    private static void type(String command, String cwd) {
         if (isBuiltin(command)) {
             System.out.println(command + " is a shell builtin");
-        } else if (locateExecutable(command) != null) {
-            System.out.println(command + " is " + locateExecutable(command).getAbsolutePath());
         } else {
-            System.out.println(command + " : not found");
+            File f = locateExecutable(command, cwd);
+            if (f != null) {
+                System.out.println(command + " is " + f.getAbsolutePath());
+            } else {
+                System.out.println(command + " : not found");
+            }
         }
         return;
     }
@@ -132,7 +201,7 @@ public class Main {
         }
         File directory = new File(command).isAbsolute() ? new File(command) : new File(dir, command);
         if (directory.isDirectory()) {
-            return directory.getAbsolutePath();
+            return directory.getCanonicalPath();
         }
         System.out.println(command + ": No such file or directory");
         return dir;
